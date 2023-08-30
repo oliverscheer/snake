208ccb7851546dc83c4fbb2b6d09e127736a5cf1
Oliver Scheer <oliver.scheer@hotmail.de>
2023-08-30 18:12:38 +0200

    added minor features

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const gridSize = 20;
 let snake = [{ x: 5, y: 5 }];
 let direction = "right";
 let food = { x: 10, y: 10 };
+let snakeLength = 1; // Initialize the snake's length
+
+let gameRunning = false; // Track if the game is running
+let gamePaused = false;  // Track if the game is paused
+
 
 function getRandomPosition() {
     return Math.floor(Math.random() * gridSize);
@@ -26,6 +31,13 @@ function drawFood() {
 function update() {
     const head = { ...snake[0] };
 
+
+    // Check for collision with walls
+    if (head.x < 0 || head.x >= canvas.width / gridSize || head.y < 0 || head.y >= canvas.height / gridSize) {
+        endGame(); // Call the function to end the game
+        return;
+    }
+
     switch (direction) {
         case "up":
             head.y--;
@@ -43,18 +55,36 @@ function update() {
 
     snake.unshift(head);
 
+
     if (head.x === food.x && head.y === food.y) {
         food = { x: getRandomPosition(), y: getRandomPosition() };
+        snakeLength++; // Increment the snake's length
+        updateSnakeLength(); // Call the function to update the displayed length
     } else {
         snake.pop();
     }
 }
 
+
+function updateSnakeLength() {
+    const snakeLengthElement = document.getElementById("snakeLength");
+    snakeLengthElement.textContent = "Snake Length: " + snakeLength;
+}
+
+
+function endGame() {
+    const gameOverElement = document.getElementById("gameOver");
+    gameOverElement.style.display = "block"; // Display the game over message
+}
+
+
 function gameLoop() {
-    drawSnake();
-    drawFood();
-    update();
-    setTimeout(gameLoop, 100);
+    if (gameRunning && !gamePaused) {
+        drawSnake();
+        drawFood();
+        update();
+        setTimeout(gameLoop, 100);
+    }
 }
 
 document.addEventListener("keydown", event => {
@@ -73,5 +103,45 @@ document.addEventListener("keydown", event => {
             break;
     }
 });
+
+
+function startGame() {
+    if (!gameRunning) {
+        gameRunning = true;
+        gameLoop();
+    }
+}
+
+function pauseGame() {
+    if (gameRunning && !gamePaused) {
+        gamePaused = true;
+    }
+}
+
+function resumeGame() {
+    if (gameRunning && gamePaused) {
+        gamePaused = false;
+        gameLoop();
+    }
+}
+
+function restartGame() {
+    gameRunning = false;
+    gamePaused = false;
+    snake = [{ x: 5, y: 5 }];
+    direction = "right";
+    snakeLength = 1;
+    food = { x: 10, y: 10 };
+    updateSnakeLength();
+    const gameOverElement = document.getElementById("gameOver");
+    gameOverElement.style.display = "none";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+document.getElementById("startButton").addEventListener("click", startGame);
+document.getElementById("pauseButton").addEventListener("click", pauseGame);
+document.getElementById("resumeButton").addEventListener("click", resumeGame);
+document.getElementById("restartButton").addEventListener("click", restartGame);
+
 
 gameLoop();
